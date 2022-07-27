0190ac29429e7643d3f79cb41173135ea65a85b6
McLovin <aurinhorodrigues@gmail.com>
2022-07-27 00:51:47 -0300

    prototipo do front end

--- js/Controller.js
+++ js/Controller.js
@@ -1,6 +1,54 @@
 export class Controller {
 
-    adicionaTarefa(tarefa){
+    listarTarefas(){
+        let tarefas = localStorage.getItem('tarefas')
+        tarefas = JSON.parse(tarefas)
+        return tarefas
+    }
+
+    adicionaTarefa(nova_tarefa){
+        let tarefas = this.listarTarefas()
+        tarefas.push(nova_tarefa)
+        this.atualizaLocalStorage(tarefas)
+
+        //apenas na versão com json
+        this.adicionaTarefaServerJSON(nova_tarefa)
+    }
+
+    removeTarefa(id){
+        let tarefas = this.listarTarefas()
+        let tarefas_atualizada = tarefas.filter((tarefa) => tarefa.id != id)
+        this.atualizaLocalStorage(tarefas_atualizada)
+
+        //apenas na versão com json
+        this.removeTarefaServerJSON(id)
+
+
+    }
+    
+    atualizaTarefa(tarefa){
+        this.removeTarefa(tarefa.id)
+        this.adicionaTarefa(tarefa)
+
+        //apenas na versão com json
+        this.atualizaTarefaServerJSON(tarefa)
+    }
+
+    atualizaLocalStorage(tarefas){
+        localStorage.clear()
+        localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    }
+
+    getId(){
+        let id = this.listarTarefas().length
+        return id++
+    }
+
+    //Métodos do JSON Server
+
+    adicionaTarefaServerJSON(tarefa){
+
+        delete tarefa.id
 
         fetch("http://localhost:3000/tarefas", {
             method: "POST",
@@ -8,10 +56,21 @@ export class Controller {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(tarefa)
+        }).then(res => res.json())  
+    }
+
+    removeTarefaServerJSON(id){
+
+        fetch(`http://localhost:3000/tarefas/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
         }).then(res => res.json())
     }
 
-    atualizaTarefa(tarefa){
+    atualizaTarefaServerJSON(tarefa){
+
         fetch("http://localhost:3000/tarefas", {
             method: "PUT",
             headers: {
@@ -19,56 +78,8 @@ export class Controller {
             },
             body: JSON.stringify(tarefa)
         }).then(res => res.json())
-
-        this.atualizaLocalStorage()
-
-    }
-
-    removeTarefa(id){
-        fetch(`http://localhost:3000/tarefas/${id}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        }).then(res => res.json())
-
-        this.atualizaLocalStorage()
-    }
-
-    populaServerJSON(){
-        let tarefas = localStorage.getItem('tarefas')
-        tarefas = JSON.parse(tarefas)
-
-        tarefas.forEach(tarefa => {
-
-            delete tarefa.id
-
-            fetch("http://localhost:3000/tarefas", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(tarefa)
-        }).then(res => res.json())
-        })
-        
-    }
-
-    async listasTarefas(){
-        return await fetch('http://localhost:3000/tarefas')
-            .then(res => res.json())
-            .then(tarefas => tarefas)
-    }
-
-    async atualizaLocalStorage(){
-        let tarefas = await fetch('http://localhost:3000/tarefas')
-            .then(res => res.json())
-            .then(tarefas => tarefas)
-        
-        localStorage.clear()
-
-        localStorage.setItem('tarefas', JSON.stringify(tarefas))
-
     }
 
 }
+
+    
--- js/View.js
+++ js/View.js
@@ -1,33 +1,18 @@
 import { Controller } from "./Controller.js";
 
-let $ = document.querySelector.bind(document)
-
-let controller = new Controller
-
-controller.populaServerJSON()
-
-$('#btn-add-tarefa').addEventListener('click', () => iniciaModal())
-$("#texto-empty-note").addEventListener('click', () => iniciaModal())
-$("#img-empty-note").addEventListener('click', () => iniciaModal())
-
-function iniciaModal(){
-    const modal = $('#modal-container')
-    modal.style.visibility = 'visible'
-    
-    $('#btn-modal-can').addEventListener('click', (e) =>{
-        e.preventDefault()
-        modal.style.visibility = 'hidden'
-    })
+if (!localStorage.getItem('tarefas')) {
+    localStorage.setItem('tarefas', JSON.stringify([]))
 }
 
+let $ = document.querySelector.bind(document)
+let controller = new Controller
 
 
 const form = $('#form')
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const { titulo, descricao, data, categoria } = 
-    e.target
+    const { titulo, descricao, data, categoria } = e.target
 
     const nova_tarefa = {
         titulo: titulo.value,
@@ -36,30 +21,32 @@ form.addEventListener('submit', async (e) => {
         categoria: categoria.value
     }
 
-   await controller
-   .adicionaTarefa(nova_tarefa)
-   .then(() => {
-        titulo.value = ''
-        descricao.value = ''
-        data.value = ''
-        categoria.value = ''
-        console.log(titulo.value)
-    })
-   //.then(() => location.reload())
-
-   
+    controller.adicionaTarefa(nova_tarefa)
 
 })
 
 
+$('#btn-add-tarefa').addEventListener('click', () => iniciaModal())
+$("#texto-empty-note").addEventListener('click', () => iniciaModal())
+$("#img-empty-note").addEventListener('click', () => iniciaModal())
 
-async function confereEmptyImg(){
-    let tarefas = await controller.listasTarefas()
-    if(tarefas.length == 0){
-        $('#empty-note').style.visibility = "visible"
-    } else {
-        $('#empty-note').style.visibility = "hidden"
-    } 
+function iniciaModal() {
+
+    const modal = $('#modal-container')
+    modal.style.visibility = 'visible'
+
+    $('#btn-modal-can').addEventListener('click', (e) => {
+        e.preventDefault()
+        modal.style.visibility = 'hidden'
+    })
 }
 
+function confereEmptyImg() {
+    let tarefas = localStorage.getItem('tarefas')
+    if (!JSON.parse(tarefas).length) {
+        $('#empty-note').style.display = 'flex'
+    } else {
+        $('#empty-note').style.display = 'none'
+    }
+}
 confereEmptyImg()
